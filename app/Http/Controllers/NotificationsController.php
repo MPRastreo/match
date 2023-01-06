372bb53919de8cfee6f579bcc57405e918b99210
Cris1816 <dev02@rastreoporsatelite.com.mx>
2023-01-06 15:05:22 -0600

    Cambios
    Notificaciones traducidas

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -7,7 +7,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use JoggApp\GoogleTranslate\GoogleTranslate;
+use GoogleTranslate;
 use stdClass;
 
 class NotificationsController extends Controller
@@ -19,18 +19,26 @@ class NotificationsController extends Controller
      */
     public function index()
     {
+        $notificationsTranslated = [];
         if (Auth::user()->role == 2 || Auth::user()->role == 1) {
-
-            Log::info("Si entro");
-
-            Log::info(Auth::user()->_id);
-
             $notificaciones = Notifications::where('id_usuario', '=', Auth::user()->_id)->get();
+
+            for ($i = 0; $i < sizeof($notificaciones); $i++)
+            {
+                $notificaciones[$i]->titulo = ucwords(GoogleTranslate::justTranslate($notificaciones[$i]->titulo, app()->getLocale()));
+                $notificaciones[$i]->cuerpo = ucwords(GoogleTranslate::justTranslate($notificaciones[$i]->cuerpo, app()->getLocale()));
+            }
 
             return response()->json($notificaciones);
 
         }elseif (Auth::user()->role == 4) {
             $notificaciones = Notifications::where('visto',false)->where('id_usuario','Informativa')->get();
+
+            for ($i = 0; $i < sizeof($notificaciones); $i++)
+            {
+                $notificaciones[$i]->titulo = ucwords(GoogleTranslate::justTranslate($notificaciones[$i]->titulo, app()->getLocale()));
+                $notificaciones[$i]->cuerpo = ucwords(GoogleTranslate::justTranslate($notificaciones[$i]->cuerpo, app()->getLocale()));
+            }
 
             return response()->json($notificaciones);
         }
@@ -112,8 +120,8 @@ class NotificationsController extends Controller
             $obj = new stdClass();
 
             $obj->encabezado = 'Match';
-            $obj->titulo = ucwords(GoogleTranslate::justTranslate('Quotation', app()->getLocale()));
-            $obj->cuerpo = ucwords(GoogleTranslate::justTranslate('You have an appointment to assign', app()->getLocale()));
+            $obj->titulo = 'quotation';
+            $obj->cuerpo = 'You have an appointment to assign.';
             $notificaciones->visto = false;
             $notificaciones->id_usuario = "Informativa";
 
@@ -129,8 +137,8 @@ class NotificationsController extends Controller
             $objUser = new stdClass();
 
             $objUser->encabezado = 'Match';
-            $objUser->titulo = ucwords(GoogleTranslate::justTranslate('Quotation', app()->getLocale()));
-            $objUser->cuerpo = ucwords(GoogleTranslate::justTranslate('Your appointment is pending assignment.', app()->getLocale()));
+            $objUser->titulo = 'quotation';
+            $objUser->cuerpo = 'Your appointment is pending assignment.';
             $notificacionesUser->visto = false;
             $notificacionesUser->id_usuario = $user->familyMemberID;
 
@@ -152,8 +160,8 @@ class NotificationsController extends Controller
             $obj = new stdClass();
 
             $obj->encabezado = 'Match';
-            $obj->titulo = ucwords(GoogleTranslate::justTranslate('Quotation', app()->getLocale()));
-            $obj->cuerpo = ucwords(GoogleTranslate::justTranslate('Your appointment is already assigned', app()->getLocale()));
+            $obj->titulo = 'quotation';
+            $obj->cuerpo = 'Your appointment is already assigned.';
             $obj->visto = false;
             $obj->id_usuario = $user->familyMemberID;
 
